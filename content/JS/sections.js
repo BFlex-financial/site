2290a94d475c2e9e1d08aa488cea73fa5a1b37a9
Hector2523 <67517312+Hector2523@users.noreply.github.com>
2024-09-07 20:56:58 +0000

    about

--- content/JS/sections.js
+++ content/JS/sections.js
@@ -1,6 +1,8 @@
-const sections = ["home", "about"];
+const sections = ["home", "about", "developers"];
+const header = document.getElementById('header');
 const homeBtn = document.getElementById('homeBtn');
 const aboutBtn = document.getElementById('aboutBtn');
+const devsBtn = document.getElementById('devsBtn');
 var observer;
 var run = true;
 
@@ -9,7 +11,9 @@ function initializeObserver(...v) {
     observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
 
-            if (entry.target.id == 'home' && entry.intersectionRatio > 0.5) {
+            if (entry.target.id == 'home' && entry.intersectionRatio > 0.2) {
+                header.classList.remove('dark');
+                devsBtn.classList.remove('marked');
                 aboutBtn.classList.remove('marked');
                 homeBtn.classList.add('marked');
                 return 0;
@@ -17,7 +21,17 @@ function initializeObserver(...v) {
 
             if (entry.target.id == 'about' && entry.intersectionRatio > 0.5) {
                 homeBtn.classList.remove('marked');
+                devsBtn.classList.remove('marked');
                 aboutBtn.classList.add('marked');
+                header.classList.add('dark');
+                return 0;
+            }
+
+            if (entry.target.id == 'developers' && entry.intersectionRatio > 0.85) {
+                header.classList.remove('dark');
+                homeBtn.classList.remove('marked');
+                aboutBtn.classList.remove('marked');
+                devsBtn.classList.add('marked');
                 return 0;
             }
 
@@ -36,8 +50,8 @@ function initializeObserver(...v) {
     })
 }
 
-window.addEventListener('DOMContentLoaded', () => { setTimeout(() => {initializeObserver(homeBtn, aboutBtn)}, 500) });
-window.addEventListener('scroll', initializeObserver(homeBtn, aboutBtn));
+window.addEventListener('DOMContentLoaded', () => { setTimeout(() => {initializeObserver(homeBtn, aboutBtn, devsBtn)}, 500) });
+window.addEventListener('scroll', initializeObserver(homeBtn, aboutBtn, devsBtn));
 
 function scrollSection(section) {
     if (run == true && section !== undefined) {
