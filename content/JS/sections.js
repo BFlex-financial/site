ad600bb7cc177dbf70de2be13a442b628b6b7dcd
Hector2523 <67517312+Hector2523@users.noreply.github.com>
2024-09-07 17:24:21 +0000

    Pending changes exported from your codespace

--- content/JS/sections.js
+++ content/JS/sections.js
@@ -9,10 +9,18 @@ function initializeObserver(...v) {
         entries.forEach(entry => {
 
             if (entry.target.id == 'home' && entry.intersectionRatio > 0.5) {
-                console.log(entry.intersectionRatio)
+                aboutBtn.classList.remove('marked');
+                homeBtn.classList.add('marked');
+                return 0;
             }
 
-            console.log(entry)
+            if (entry.target.id == 'about' && entry.intersectionRatio > 0.5) {
+                homeBtn.classList.remove('marked');
+                aboutBtn.classList.add('marked');
+                return 0;
+            }
+
+            return 0;
         })
     }, { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] })
 
@@ -27,5 +35,24 @@ function initializeObserver(...v) {
     })
 }
 
-window.addEventListener('DOMContentLoaded', initializeObserver(homeBtn, aboutBtn));
+window.addEventListener('DOMContentLoaded', setTimeout(() => { initializeObserver(homeBtn, aboutBtn) }, 500));
 window.addEventListener('scroll', initializeObserver(homeBtn, aboutBtn));
+
+async function scroll(section) {
+    if (run == true && section !== undefined) {
+        run = false;
+
+        await switch (section) {
+            case 0:
+                document.getElementById(sections[section]).scrollIntoView();
+                break
+            case 1:
+                document.getElementById(sections[section]).scrollIntoView();
+                break
+            case 2:
+                document.getElementById(sections[section]).scrollIntoView();
+                break
+        }
+    }
+
+}
